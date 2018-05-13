b7a4d500c0fabb85dd7e7fcfd9d9379ba42e5cf6
Michael Jeffrey <MichaelJeffrey@michael-jeffreys-macbook-pro.local>
2018-05-13 20:24:24 +1000

    Test

---
+++ navigationApp/navigationApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  navigationApp
+//
+//  Created by Michael Jeffrey on 13/5/18.
+//  Copyright © 2018 Michael Jeffrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ navigationApp/navigationAppTests/navigationAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  navigationAppTests.swift
+//  navigationAppTests
+//
+//  Created by Michael Jeffrey on 13/5/18.
+//  Copyright © 2018 Michael Jeffrey. All rights reserved.
+//
+
+import XCTest
+@testable import navigationApp
+
+class navigationAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
